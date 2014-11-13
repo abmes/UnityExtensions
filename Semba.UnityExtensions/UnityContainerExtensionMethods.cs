692fe7f1b4131b15034f2f7c71792602ee54baea
stefan <stefan@semba.biz>
2014-11-13 10:02:44 +0200

    test commit from stefan

--- Semba.UnityExtensions/UnityContainerExtensionMethods.cs
+++ Semba.UnityExtensions/UnityContainerExtensionMethods.cs
@@ -24,5 +24,6 @@ namespace Semba.UnityExtensions
         {
             return container.RegisterType(typeof(IEnumerable<>), new InjectionFactory((c, t, n) => c.ResolveAll(t.GetGenericArguments().Single())));
         }
+		
     }
 }
