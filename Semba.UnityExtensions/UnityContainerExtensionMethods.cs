4d87559bf5a909ef3b6b5061c51b0be4d8162f32
stefan <stefan@semba.biz>
2014-12-02 10:42:26 +0200

    decorator chain extension

--- Semba.UnityExtensions/UnityContainerExtensionMethods.cs
+++ Semba.UnityExtensions/UnityContainerExtensionMethods.cs
@@ -24,35 +24,95 @@ namespace Semba.UnityExtensions
             return container.RegisterType(typeof(IEnumerable<>), new InjectionFactory((c, t, n) => c.ResolveAll(t.GetGenericArguments().Single())));
         }
 
-        public static IUnityContainer RegisterDecoratorChain<TFrom>(this IUnityContainer container, params Type[] decoratorChain)
+        public static IUnityContainer RegisterDecoratorChain<TFrom>(this IUnityContainer container, LifetimeManager lifetimeManager, params Type[] decoratorChain)
         {
             string previousRegistrationName = null;
 
             foreach (var t in decoratorChain)
             {
                 var currentRegistrationName = (t == decoratorChain.Last()) ? "" : Guid.NewGuid().ToString();
+                var currentLifetimeManager = (t == decoratorChain.Last()) ? lifetimeManager : new TransientLifetimeManager();
                 var constructor = t.GetConstructors().Single();
                 var parameters = constructor.GetParameters().Select(x => x.ParameterType == typeof(TFrom) ? new ResolvedParameter<TFrom>(previousRegistrationName) : new ResolvedParameter(x.ParameterType)).ToArray();
-                container.RegisterType(typeof(TFrom), t, currentRegistrationName, new InjectionConstructor(parameters));
+                container.RegisterType(typeof(TFrom), t, currentRegistrationName, currentLifetimeManager, new InjectionConstructor(parameters));
                 previousRegistrationName = currentRegistrationName;
             }
 
             return container;
         }
 
+        public static IUnityContainer RegisterDecoratorChain<TFrom>(this IUnityContainer container, params Type[] decoratorChain)
+        {
+            return container.RegisterDecoratorChain<TFrom>(new TransientLifetimeManager(), decoratorChain);
+        }
+
         public static IUnityContainer RegisterDecoratorChain<TFrom, TTo, TDecorator>(this IUnityContainer container)
+            where TTo : TFrom
+            where TDecorator : TFrom
         {
             return container.RegisterDecoratorChain<TFrom>(typeof(TTo), typeof(TDecorator));
         }
 
+        public static IUnityContainer RegisterDecoratorChain<TFrom, TTo, TDecorator>(this IUnityContainer container, LifetimeManager lifetimeManager)
+            where TTo : TFrom
+            where TDecorator : TFrom
+        {
+            return container.RegisterDecoratorChain<TFrom>(lifetimeManager, typeof(TTo), typeof(TDecorator));
+        }
+
+        public static IUnityContainer RegisterSingletonDecoratorChain<TFrom, TTo, TDecorator>(this IUnityContainer container)
+            where TTo : TFrom
+            where TDecorator : TFrom
+        {
+            return container.RegisterDecoratorChain<TFrom>(new ContainerControlledLifetimeManager(), typeof(TTo), typeof(TDecorator));
+        }
+
         public static IUnityContainer RegisterDecoratorChain<TFrom, TTo, TDecorator1, TDecorator2>(this IUnityContainer container)
+            where TTo : TFrom
+            where TDecorator1 : TFrom
+            where TDecorator2 : TFrom
         {
             return container.RegisterDecoratorChain<TFrom>(typeof(TTo), typeof(TDecorator1), typeof(TDecorator2));
         }
 
+        public static IUnityContainer RegisterDecoratorChain<TFrom, TTo, TDecorator1, TDecorator2>(this IUnityContainer container, LifetimeManager lifetimeManager)
+            where TTo : TFrom
+            where TDecorator1 : TFrom
+            where TDecorator2 : TFrom
+        {
+            return container.RegisterDecoratorChain<TFrom>(lifetimeManager, typeof(TTo), typeof(TDecorator1), typeof(TDecorator2));
+        }
+
+        public static IUnityContainer RegisterSingletonDecoratorChain<TFrom, TTo, TDecorator1, TDecorator2>(this IUnityContainer container)
+            where TTo : TFrom
+            where TDecorator1 : TFrom
+            where TDecorator2 : TFrom
+        {
+            return container.RegisterDecoratorChain<TFrom>(new ContainerControlledLifetimeManager(), typeof(TTo), typeof(TDecorator1), typeof(TDecorator2));
+        }
+
         public static IUnityContainer RegisterDecoratorChain<TFrom, TTo, TDecorator1, TDecorator2, TDecorator3>(this IUnityContainer container)
+            where TTo : TFrom
+            where TDecorator1 : TFrom
+            where TDecorator3 : TFrom
         {
             return container.RegisterDecoratorChain<TFrom>(typeof(TTo), typeof(TDecorator1), typeof(TDecorator2), typeof(TDecorator3));
+        }
+
+        public static IUnityContainer RegisterDecoratorChain<TFrom, TTo, TDecorator1, TDecorator2, TDecorator3>(this IUnityContainer container, LifetimeManager lifetimeManager)
+            where TTo : TFrom
+            where TDecorator1 : TFrom
+            where TDecorator3 : TFrom
+        {
+            return container.RegisterDecoratorChain<TFrom>(lifetimeManager, typeof(TTo), typeof(TDecorator1), typeof(TDecorator2), typeof(TDecorator3));
+        }
+
+        public static IUnityContainer RegisterSingletonDecoratorChain<TFrom, TTo, TDecorator1, TDecorator2, TDecorator3>(this IUnityContainer container)
+            where TTo : TFrom
+            where TDecorator1 : TFrom
+            where TDecorator3 : TFrom
+        {
+            return container.RegisterDecoratorChain<TFrom>(new ContainerControlledLifetimeManager(), typeof(TTo), typeof(TDecorator1), typeof(TDecorator2), typeof(TDecorator3));
         }
     }
 }
