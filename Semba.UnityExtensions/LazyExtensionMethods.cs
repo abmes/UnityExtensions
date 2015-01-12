23e078b613ed3a06dc8db86f1a1e71414d2b724a
stefan <stefan@semba.biz>
2015-01-12 10:06:00 +0200

    RegisterTypeByFactoryFuncLazy

--- Semba.UnityExtensions/LazyExtensionMethods.cs
+++ Semba.UnityExtensions/LazyExtensionMethods.cs
@@ -6,72 +6,51 @@ using Microsoft.Practices.Unity;
 
 namespace Semba.UnityExtensions
 {
+    class LazyInterceptor<T> : Castle.DynamicProxy.IInterceptor
+    {
+        private readonly Lazy<T> _lazyObject;
+
+        public LazyInterceptor(Func<T> factoryFunc)
+        {
+            _lazyObject = new Lazy<T>(factoryFunc);
+        }
+
+        public void Intercept(Castle.DynamicProxy.IInvocation invocation)
+        {
+            invocation.ReturnValue = invocation.Method.Invoke(_lazyObject.Value, invocation.Arguments);
+        }
+    }
+
     public static class LazyExtensionMethods
     {
-        class LazyInterceptor<T> : Castle.DynamicProxy.IInterceptor
+        private static T GetLazyProxy<T>(Func<T> factoryFunc) where T : class
         {
-            private readonly Lazy<T> _lazyObject;
-
-            public LazyInterceptor(Func<T> factoryFunc)
-            {
-                _lazyObject = new Lazy<T>(factoryFunc);
-            }
-
-            public void Intercept(Castle.DynamicProxy.IInvocation invocation)
-            {
-                var targetType = typeof(T);
-                var method = targetType.GetMethod(invocation.Method.Name, invocation.Method.GetParameters().Select(x => x.ParameterType).ToArray());
-                invocation.ReturnValue = method.Invoke(_lazyObject.Value, invocation.Arguments);
-            }
+            return new Castle.DynamicProxy.ProxyGenerator().CreateInterfaceProxyWithoutTarget<T>(new LazyInterceptor<T>(factoryFunc));
         }
 
-        // ------------------------------
-
-        //public static IUnityContainer RegisterLazyDecorator<TFrom>(this IUnityContainer container) where TFrom : class
-        //{
-        //    var proxyGenerator = new Castle.DynamicProxy.ProxyGenerator();
-
-        //    var toType = proxyGenerator.ProxyBuilder.CreateInterfaceProxyTypeWithoutTarget(typeof(TFrom), null, null);
-        //    //var toType = proxyGenerator.CreateInterfaceProxyWithoutTarget<TFrom>(new LazyInterceptor<TFrom>(container.Resolve<Func<TFrom>>()));
-
-        //    return container.RegisterType(typeof(TFrom), toType, new InjectionFactory(c => proxyGenerator.CreateInterfaceProxyWithoutTarget<TFrom>(new LazyInterceptor<TFrom>(c.Resolve<Func<TFrom>>()))));
-        //}
-
-        // -------------------------------
-
-        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult>(this IUnityContainer container, Func<TResult> factoryFunc) where TResult : class
+        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult>(this IUnityContainer container, Func<TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
         {
-            return container.RegisterType<TResult>(new InjectionFactory(c => new Castle.DynamicProxy.ProxyGenerator().CreateInterfaceProxyWithoutTarget<TResult>(new LazyInterceptor<TResult>(() => factoryFunc()))));
+            return container.RegisterTypeByFactoryFunc<TResult>(() => GetLazyProxy<TResult>(() => factoryFunc()) , resolvedParameters);
         }
 
-        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam>(this IUnityContainer container, Func<TParam, TResult> factoryFunc) where TResult : class
+        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam1>(this IUnityContainer container, Func<TParam1, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
         {
-            return container.RegisterType<TResult>(new InjectionFactory(c => new Castle.DynamicProxy.ProxyGenerator().CreateInterfaceProxyWithoutTarget<TResult>(new LazyInterceptor<TResult>(() => factoryFunc(c.Resolve<TParam>())))));
+            return container.RegisterTypeByFactoryFunc<TResult, Func<TParam1>>((param1Func) => GetLazyProxy<TResult>(() => factoryFunc(param1Func())), resolvedParameters);
         }
 
-        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam1, TParam2>(this IUnityContainer container, Func<TParam1, TParam2, TResult> factoryFunc) where TResult : class
+        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam1, TParam2>(this IUnityContainer container, Func<TParam1, TParam2, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
         {
-            return
-                container.RegisterType<TResult>(
-                    new InjectionFactory(c =>
-                        {
-                            var param1Func = c.Resolve<Func<TParam1>>();
-                            var param2Func = c.Resolve<Func<TParam2>>();
-
-                            return
-                                new Castle.DynamicProxy.ProxyGenerator().CreateInterfaceProxyWithoutTarget<TResult>(
-                                    new LazyInterceptor<TResult>(() => factoryFunc(param1Func(), param2Func())));
-                        }));
+            return container.RegisterTypeByFactoryFunc<TResult, Func<TParam1>, Func<TParam2>>((param1Func, param2Func) => GetLazyProxy<TResult>(() => factoryFunc(param1Func(), param2Func())), resolvedParameters);
         }
 
-        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam1, TParam2, TParam3>(this IUnityContainer container, Func<TParam1, TParam2, TParam3, TResult> factoryFunc) where TResult : class
+        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam1, TParam2, TParam3>(this IUnityContainer container, Func<TParam1, TParam2, TParam3, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
         {
-            return container.RegisterType<TResult>(new InjectionFactory(c => new Castle.DynamicProxy.ProxyGenerator().CreateInterfaceProxyWithoutTarget<TResult>(new LazyInterceptor<TResult>(() => factoryFunc(c.Resolve<TParam1>(), c.Resolve<TParam2>(), c.Resolve<TParam3>())))));
+            return container.RegisterTypeByFactoryFunc<TResult, Func<TParam1>, Func<TParam2>, Func<TParam3>>((param1Func, param2Func, param3Func) => GetLazyProxy<TResult>(() => factoryFunc(param1Func(), param2Func(), param3Func())), resolvedParameters);
         }
 
-        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam1, TParam2, TParam3, TParam4>(this IUnityContainer container, Func<TParam1, TParam2, TParam3, TParam4, TResult> factoryFunc) where TResult : class
+        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam1, TParam2, TParam3, TParam4>(this IUnityContainer container, Func<TParam1, TParam2, TParam3, TParam4, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
         {
-            return container.RegisterType<TResult>(new InjectionFactory(c => new Castle.DynamicProxy.ProxyGenerator().CreateInterfaceProxyWithoutTarget<TResult>(new LazyInterceptor<TResult>(() => factoryFunc(c.Resolve<TParam1>(), c.Resolve<TParam2>(), c.Resolve<TParam3>(), c.Resolve<TParam4>())))));
+            return container.RegisterTypeByFactoryFunc<TResult, Func<TParam1>, Func<TParam2>, Func<TParam3>, Func<TParam4>>((param1Func, param2Func, param3Func, param4Func) => GetLazyProxy<TResult>(() => factoryFunc(param1Func(), param2Func(), param3Func(), param4Func())), resolvedParameters);
         }
     }
 }
