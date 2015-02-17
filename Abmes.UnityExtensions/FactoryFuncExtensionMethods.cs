d5a22d88391d330f16ff7c6fa5b425309a07a270
stefan <stef.tashev@gmail.com>
2015-02-17 17:31:39 +0200

    added RegisterTypeByFactoryFuncLazy methods

--- Abmes.UnityExtensions/FactoryFuncExtensionMethods.cs
+++ Abmes.UnityExtensions/FactoryFuncExtensionMethods.cs
@@ -14,9 +14,19 @@ namespace Abmes.UnityExtensions
             return container.RegisterTypeEx<TResult>().ByFactoryFunc(factoryFunc);
         }
 
+        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult>(this IUnityContainer container, Func<TResult> factoryFunc) where TResult : class
+        {
+            return container.RegisterTypeEx<TResult>().ByFactoryFuncLazy(factoryFunc);
+        }
+
         public static IUnityContainer RegisterTypeByFactoryFunc<TResult, TParam1>(this IUnityContainer container, Func<TParam1, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
         {
             return container.RegisterTypeEx<TResult>().ByFactoryFunc<TParam1>(factoryFunc, resolvedParameters);
+        }
+
+        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam1>(this IUnityContainer container, Func<TParam1, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
+        {
+            return container.RegisterTypeEx<TResult>().ByFactoryFuncLazy<TParam1>(factoryFunc, resolvedParameters);
         }
 
         public static IUnityContainer RegisterTypeByFactoryFunc<TResult, TParam1, TParam2>(this IUnityContainer container, Func<TParam1, TParam2, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
@@ -24,14 +34,29 @@ namespace Abmes.UnityExtensions
             return container.RegisterTypeEx<TResult>().ByFactoryFunc<TParam1, TParam2>(factoryFunc, resolvedParameters);
         }
 
+        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam1, TParam2>(this IUnityContainer container, Func<TParam1, TParam2, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
+        {
+            return container.RegisterTypeEx<TResult>().ByFactoryFuncLazy<TParam1, TParam2>(factoryFunc, resolvedParameters);
+        }
+
         public static IUnityContainer RegisterTypeByFactoryFunc<TResult, TParam1, TParam2, TParam3>(this IUnityContainer container, Func<TParam1, TParam2, TParam3, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
         {
             return container.RegisterTypeEx<TResult>().ByFactoryFunc<TParam1, TParam2, TParam3>(factoryFunc, resolvedParameters);
         }
 
+        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam1, TParam2, TParam3>(this IUnityContainer container, Func<TParam1, TParam2, TParam3, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
+        {
+            return container.RegisterTypeEx<TResult>().ByFactoryFuncLazy<TParam1, TParam2, TParam3>(factoryFunc, resolvedParameters);
+        }
+
         public static IUnityContainer RegisterTypeByFactoryFunc<TResult, TParam1, TParam2, TParam3, TParam4>(this IUnityContainer container, Func<TParam1, TParam2, TParam3, TParam4, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
         {
             return container.RegisterTypeEx<TResult>().ByFactoryFunc<TParam1, TParam2, TParam3, TParam4>(factoryFunc, resolvedParameters);
+        }
+
+        public static IUnityContainer RegisterTypeByFactoryFuncLazy<TResult, TParam1, TParam2, TParam3, TParam4>(this IUnityContainer container, Func<TParam1, TParam2, TParam3, TParam4, TResult> factoryFunc, params ResolvedParameter[] resolvedParameters) where TResult : class
+        {
+            return container.RegisterTypeEx<TResult>().ByFactoryFuncLazy<TParam1, TParam2, TParam3, TParam4>(factoryFunc, resolvedParameters);
         }
     }
 }
