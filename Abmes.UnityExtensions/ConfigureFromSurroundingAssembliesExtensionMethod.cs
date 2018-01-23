a892db54ab747308db2f267834042963582870e5
Lyubomir Tonchev <lubo@semba.bg>
2018-01-23 14:24:37 +0200

    unity 5

--- Abmes.UnityExtensions/ConfigureFromSurroundingAssembliesExtensionMethod.cs
+++ Abmes.UnityExtensions/ConfigureFromSurroundingAssembliesExtensionMethod.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.Practices.Unity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using Unity;
 
 namespace Abmes.UnityExtensions
 {
